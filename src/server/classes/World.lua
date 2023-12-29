6f7c07b23a522dd7474b2c6e0a862ec23dd9d4a4
Alex Arpie <119821293+Devallex@users.noreply.github.com>
2023-12-29 01:12:48 -0500

    Improved World example

--- src/server/classes/World.lua
+++ src/server/classes/World.lua
@@ -14,14 +14,25 @@ World.worldName = "Default World"
 World.terrainSize = 0
 World.isGenerated = false
 
+-- This is automatically called when World.new(...) or World(...) is run, passing paramaters as well
 function World:__init(worldName, terrainSize, ...)
-	-- Because it's being overwritten,
-	-- you need to call this to inherit it's behavior
-	Emitter.__init(self, worldName, terrainSize, ...)
+	-- Because it's being overwritten, you need to call this to inherit it's behavior.
+	Emitter.__init(self, worldName, terrainSize, ...) -- The Emitter doesn't neccecarily use these paramaters, but it's a good practice to send them anyway
 
 	-- You can update properties here
 	self.worldName = worldName
 	self.terrainSize = terrainSize
+	-- ( And you can also set default properties here too )
+
+	-- This is a good place to listen for Emitter events. (Outside of the class works too!)
+	self:on("clear", function()
+		print("The world was cleared!")
+	end)
+end
+
+-- This is automatically called when World:create(className, ...) is run, passing paramaters (except for className)
+function World:__create(...)
+	print("A new class is inheriting directly from this one to be created!")
 end
 
 function World:generate()
@@ -37,6 +48,31 @@ function World:generate()
 
 	self.isGenerated = true -- Update a property
 	self:call("generate", self.terrainSize) -- Calls an Emitter event
+end
+
+-- A "static" function, not specific to any fixed amount of World instances
+-- This uses iteration functions, which preforms bulk-actions on multiple instances of this class
+function World.clearAll()
+	-- This method:
+	World:iterAll("clear")
+
+	-- ...is identical to:
+	World:iterCall(function(self)
+		self:clear()
+	end)
+
+	-- ...and also identical too:
+	for self in World:iter() do
+		self:clear()
+	end
+
+	-- ...as well as:
+	for _, self in ipairs(World:getInstances()) do
+		self:clear()
+	end
+
+	-- They all do the same thing, call the "clear" method on every instance of the class.
+	-- (Just don't call these all at once, or it'll run multiple times.)
 end
 
 function World:clear()
