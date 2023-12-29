17f5bb3265c9b9186a6f439bd6ed9d9d42eb6379
Alex Arpie <119821293+Devallex@users.noreply.github.com>
2023-12-29 03:14:51 -0500

    Update setupManager to require example script

--- src/server/scripts/setupManager.server.lua
+++ src/server/scripts/setupManager.server.lua
@@ -4,20 +4,5 @@ require(game:GetService("ReplicatedStorage"):WaitForChild("modules"):WaitForChil
 -- Do other stuff here, such as requiring other scripts.
 -- _G("scripts.somethingManager")
 
--- # EXAMPLE FROM DOCUMENTATION #
-
--- Initialize the Replicator
-_G("classes.Replicator")
-
-local World = _G("classes.World")
-
--- Create a new instance/copy of World
--- You can also do World:new(...)
-local world = World("Epic World", 50)
-
--- Receives an emitter event
-world:on("generate", function(size)
-	print("The world has been generated with a size of "..size)
-end)
-
-world:generate()
+-- Run the example scripts
+_G("scripts.examples.worldManager")
